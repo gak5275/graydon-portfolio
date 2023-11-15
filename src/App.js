67a2627bd9fb92513dd4745caa3f93a0b9ec4371
gak5275 <gak5275@psu.edu>
2023-11-15 17:15:40 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import murray from './images/murray.png';
+import { useState } from 'react';
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+      <button onClick={handleClick}>
+        Clicked {count} times
+      </button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Graydon Kupfer's Digital Portfolio</h1>
         <img src={murray} className="App-logo" alt="cat" />
+        <MyButton />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,5 +39,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
