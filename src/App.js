b2bc3279c0594c087c2747e8794c790f4eae314d
gak5275 <gak5275@psu.edu>
2023-11-15 16:41:31 -0500

    added Murray

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import murray from './images/murray.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={murray} className="App-logo" alt="cat" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
