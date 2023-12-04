aa261157ce71c780f6295cf838b5b20d58faf892
gak5275 <gak5275@psu.edu>
2023-12-04 16:49:44 -0500

    added stardew link

--- src/pages/stardew.js
+++ src/pages/stardew.js
@@ -9,7 +9,7 @@ const Stardew = () => {
             </h1>
             <img src={Main_Logo} alt="Stardew Valley Logo" />
             <p>Myself and two classmates created a website on which we share our analysis of the video game Stardew Valley. We looked at the text in the game's code and found recurring themes in the dialogue. We also looked at which characters have the most lines of dialogue in specific locations throughout the game.</p>
-            <p>View the site here!</p>
+            <p><a href="https://gak5275.github.io/StardewDIGIT210/">View the site here!</a></p>
         </div>
     );
 };
